a43b1e0dfe2399db671b38e7eb5a9693a6164130
jarvisi365 <i365jarvis@gmail.com>
2026-04-23 19:29:24 +0000

    fix(landing): restore translation dictionaries for english and portuguese

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -4,8 +4,16 @@ import { initReactI18next } from "react-i18next";
 import landingES from "./locales/es/landing.json";
 import commonES from "./locales/es/common.json";
 
+import landingEN from "./locales/en/landing.json";
+import commonEN from "./locales/en/common.json";
+
+import landingPT from "./locales/pt/landing.json";
+import commonPT from "./locales/pt/common.json";
+
 const resources = {
-  es: { landing: landingES, common: commonES }
+  es: { landing: landingES, common: commonES },
+  en: { landing: landingEN, common: commonEN },
+  pt: { landing: landingPT, common: commonPT }
 };
 
 i18n.use(initReactI18next).init({
